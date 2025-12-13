4c4175a6488254c58e5d90b2e16c57f3087e0964
안순호 <ibfwbo1010@github.com>
2025-12-13 16:38:01 +0900

    Flask demand

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    idols = [
+        {"name": "BTS", "group": "방탄소년단"},
+        {"name": "BLACKPINK", "group": "블랙핑크"},
+        {"name": "EXO", "group": "엑소"},
+        {"name": "IVE", "group": "아이브"},
+        {"name": "NewJeans", "group": "뉴진스"},
+    ]
     return render_template("index.html")
 
 if __name__ == "__main__":
